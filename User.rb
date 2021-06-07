28a4596cd0e890a5ae90b3f2149ce4e9b41cd96c
Sutulov <sav.xtx@gmail.com>
2021-06-07 17:06:43 +0500

    space

--- User.rb
+++ User.rb
@@ -1,12 +1,13 @@
 class User
-	def fio
-		'Иванов Иван'
-	end
-	def profession
-		'Программист'
-	end
+  def fio
+    'Иванов Иван'
+  end
+  def profession
+    'Программист'
+  end
 end
 
 user1 = User.new
 p 'Пользователь: ' + user1.fio
 p 'Работает по профессии: ' + user1.profession
+ 
