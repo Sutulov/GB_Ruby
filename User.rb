b5bbabbd9062824cd1d82e0cc83837cf55a35111
Sutulov <sav.xtx@gmail.com>
2021-06-07 09:03:27 +0500

    Домашние задание 2.3.0

---
+++ User.rb
@@ -0,0 +1,12 @@
+class User
+	def fio
+		'Иванов Иван'
+	end
+	def profession
+		'Программист'
+	end
+end
+
+user1 = User.new
+p 'Пользователь: ' + user1.fio
+p 'Работает по профессии: ' + user1.profession
