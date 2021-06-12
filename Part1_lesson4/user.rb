a7b268b0eb1ade132fbe7bb6ecd0ef34dce94fae
Sutulov <sav.xtx@gmail.com>
2021-06-12 22:41:15 +0500

    #w

--- Part1_lesson4/user.rb
+++ Part1_lesson4/user.rb
@@ -14,11 +14,16 @@ class User
   def greed
     0..1000
   end
+
+  def snacks
+    %w[попкорн спрайт]
+  end
 end
 
 u = User.new
 # puts "ФИО: #{u.name}"
 # puts "Возраст: #{u.age}"
 # puts "Наличие работы: #{u.job}"
-puts "Допустима ли цена в 500 рублей: #{u.greed.include? 500}"
-puts "Допустима ли цена в 3500 рублей: #{u.greed.include? 3_500}"
+# puts "Допустима ли цена в 500 рублей: #{u.greed.include? 500}"
+# puts "Допустима ли цена в 3500 рублей: #{u.greed.include? 3_500}"
+puts "#{u.name} предпочитает #{u.snacks.join(', ')}"
