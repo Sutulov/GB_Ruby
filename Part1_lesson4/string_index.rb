e6d7347e81d59d28cc66d69c7f91c0efa374ae0d
Sutulov <sav.xtx@gmail.com>
2021-06-12 17:21:44 +0500

    string

---
+++ Part1_lesson4/string_index.rb
@@ -0,0 +1,11 @@
+str = 'Иванов Иван'
+puts str[4] 
+puts str.size
+puts str.length
+puts str[-4..-2]
+str[7..10] = 'Пётр'
+puts str
+str[7..8] = ''
+puts str
+str.sub('Иван', 'Пётр')
+str.пsub('Иван', 'Пётр')
