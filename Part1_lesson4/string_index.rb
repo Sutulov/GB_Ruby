25c2727fa4bd5331d86c599e2500298dbaf043d9
Sutulov <sav.xtx@gmail.com>
2021-06-12 17:26:51 +0500

    final

--- Part1_lesson4/string_index.rb
+++ Part1_lesson4/string_index.rb
@@ -3,9 +3,9 @@ puts str[4]
 puts str.size
 puts str.length
 puts str[-4..-2]
-str[7..10] = 'Пётр'
+# str[7..10] = 'Пётр'
+# puts str
+# str[7..8] = ''
 puts str
-str[7..8] = ''
-puts str
-str.sub('Иван', 'Пётр')
-str.gsub('Иван', 'Пётр')
+puts str.sub('Иван', 'Пётр')
+puts str.gsub('Иван', 'Пётр')
