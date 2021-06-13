8fc2bec7d69f9de7f51a77deb47bdda49041e1a5
Sutulov <sav.xtx@gmail.com>
2021-06-13 16:27:12 +0500

    use OpenStruct

--- Part1_lesson4/distanse.rb
+++ Part1_lesson4/distanse.rb
@@ -3,3 +3,8 @@
 a = { x: 3, y: 7 }
 b = { x: -1, y: 5 }
 puts ((b[:x] - a[:x])**2 + (b[:y] - a[:y])**2)**0.5
+
+require 'ostruct'
+a = OpenStruct.new(x: 3, y: 7)
+b = OpenStruct.new(x: -1, y: 5)
+puts ((b.x - a.x)**2 + (b.y - a.y)**2)**0.5
