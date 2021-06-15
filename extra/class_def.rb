7992225b6049d3aed70c773a70505db24c5a6853
Sutulov <sav.xtx@gmail.com>
2021-06-15 18:46:18 +0500

    respond_to?

--- extra/class_def.rb
+++ extra/class_def.rb
@@ -14,4 +14,10 @@ g = Greeter.new('Pat')
 g.say_hi
 g.say_bye
 # g.@name
-puts Greeter.instance_methods(false)
+# puts Greeter.instance_methods(false)
+puts 'g.respond_to?('name')'
+g.respond_to?('name')
+puts "g.respond_to?('say_hi')"
+g.respond_to?('say_hi')
+puts "g.respond_to?('to_s')"
+g.respond_to?('to_s')
