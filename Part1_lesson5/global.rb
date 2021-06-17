86cc22ce825caa156ce9857db3e1970d1fd7ec5d
Sutulov <sav.xtx@gmail.com>
2021-06-17 12:10:11 +0500

    use global var

---
+++ Part1_lesson5/global.rb
@@ -0,0 +1,16 @@
+def say_bye
+  $x = 'переменная $х из say_bye'
+end
+
+def start
+  $x = 'переменная $х из start'
+  puts $x
+  say_bye
+  puts $x
+end
+
+$x = 'переменная из голобальной области видимости'
+puts $x
+start
+puts $x
+puts local_variables
