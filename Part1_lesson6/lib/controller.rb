b9d7e18d5b4e648d02529ba04bb94ebadac53597
Sutulov <sav.xtx@gmail.com>
2021-06-22 21:21:24 +0500

    add check date and user name

--- Part1_lesson6/lib/controller.rb
+++ Part1_lesson6/lib/controller.rb
@@ -5,5 +5,17 @@ class Controller
     else
       puts 'Билет не обнаружен'
     end
+
+    if user.ticket.name == user.name
+      puts 'Билет на имя пользователя'
+    else
+      puts 'Билет на чужое имя'
+    end
+
+    if user.ticket.date == Date.today
+      puts 'Билет на сегодняшнюю дату'
+    else
+      puts 'Билет на другую дату'
+    end
   end
 end
