082724b4eee4d49ccf53b1ea362afbec8210a3a5
Sutulov <sav.xtx@gmail.com>
2021-06-22 20:27:09 +0500

    set_

--- Part1_lesson6/lib/ticket.rb
+++ Part1_lesson6/lib/ticket.rb
@@ -1,8 +1,12 @@
 class Ticket
+
   attr_accessor :name, :date
 
-  def initialize(name, date)
+  def set_date(name)
     @name = name
+  end
+  
+  def set_date(date)
     @date = date
   end
 end
--- Part1_lesson6/ticket_use.rb
+++ Part1_lesson6/ticket_use.rb
@@ -1,2 +1,9 @@
-require './lib/ticket'
-p Ticket.new
+require 'date'
+require_relative './lib/ticket'
+
+ticket = Ticket.new
+ticket.set_name('Иванов Иван Иванович')
+ticket.set_date(Date.new(2021, 10, 30))
+
+puts ticket.name
+puts ticket.date
