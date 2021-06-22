839e09ba8a05feb73f62b882a2888d00da3e5b27
Sutulov <sav.xtx@gmail.com>
2021-06-22 21:14:05 +0500

    final version

---
+++ Part1_lesson6/main.rb
@@ -0,0 +1,18 @@
+require 'date'
+require_relative './lib/ticket'
+require_relative './lib/user'
+require_relative './lib/controller'
+
+ticket = Ticket.new
+ticket.set_name('Иванов Иван Иванович')
+ticket.set_date(Date.new(2021, 10, 30))
+
+user = User.new
+user.set_name('Иванов Иван Иванович')
+user.set_ticket(ticket)
+
+controller = Controller.new
+controller.check(user)
+
+puts user.name
+puts user.ticket.date
