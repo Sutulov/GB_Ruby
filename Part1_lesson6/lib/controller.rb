f1327fe90e7c36b9fa1b5c23892e97dbdb89e19d
Sutulov <sav.xtx@gmail.com>
2021-06-22 21:49:06 +0500

    correct

--- Part1_lesson6/lib/controller.rb
+++ Part1_lesson6/lib/controller.rb
@@ -1,20 +1,28 @@
 class Controller
   def check(user)
+    @user = user	
     if user.ticket
       puts 'Билет в наличие'
-      if user.ticket.name == user.name
-        puts 'Билет на имя пользователя'
-      else
-        puts 'Билет на чужое имя'
-      end
-
-      if user.ticket.date == Date.today
-        puts 'Билет на сегодняшнюю дату'
-      else
-        puts 'Билет на другую дату'
-      end
+      check_name
+      check_date
     else
       puts 'Билет не обнаружен'
-    end   
+    end
+  end
+
+  def check_name
+    if @user.ticket.name == @user.name
+      puts 'Билет на имя пользователя'
+    else
+      puts 'Билет на чужое имя'
+    end
+  end
+  
+  def check_date
+    if @user.ticket.date == Date.today
+      puts 'Билет на сегодняшнюю дату'
+    else
+      puts 'Билет на другую дату'
+    end
   end
 end
