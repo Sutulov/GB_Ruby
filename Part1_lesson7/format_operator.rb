558d8c76f3ad8245b5ce4ff7e4e97573f1b78c4e
Sutulov <sav.xtx@gmail.com>
2021-06-23 20:52:20 +0500

    %..

--- Part1_lesson7/format_operator.rb
+++ Part1_lesson7/format_operator.rb
@@ -1,1 +1,17 @@
 puts "Стоимость %d рублей" % 500
+
+number = 4597
+
+puts '%b' % number
+puts '%b' % -number
+puts '%#b' % number
+puts '%#B' % number
+puts '%d' % number
+puts '%f' % number
+puts '%o' % number
+puts '%o' % -number
+puts '%#o' % number
+puts '%s' % number
+puts '%x' % number
+puts '%x' % -number
+puts '%X' % number
