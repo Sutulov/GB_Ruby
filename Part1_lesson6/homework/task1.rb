57d64fd31322ec25f2e4b85d5fb22b6e2e2ce4a3
Sutulov <sav.xtx@gmail.com>
2021-06-23 10:57:53 +0500

    array of week days

---
+++ Part1_lesson6/homework/task1.rb
@@ -0,0 +1,11 @@
+MONDAY = 'Понедельник'
+TUESDAY = 'Вторник'
+WEDNESDAY = 'Среда'
+THURSDAY = 'Четверг'
+FRIDAY = 'Пятница'
+SATURDAY = 'Суббота'
+SUNDAY = 'Воскресенье'
+WEEK = [MONDAY, TUESDAY, WEDNESDAY, THURSDAY, FRIDAY,
+SATURDAY, SUNDAY]
+
+puts WEEK
