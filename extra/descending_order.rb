3462adc212aa37ca1d48940ccd512cc10b8fd7b4
Sutulov <sav.xtx@gmail.com>
2021-06-24 21:29:27 +0500

    not good

--- extra/descending_order.rb
+++ extra/descending_order.rb
@@ -2,7 +2,11 @@ def descending_order(num)
   num.to_s
   arr = []
   (0..num.size-1).each do |i|
-  	arr.push(num[i])
+    arr.push(num[i])
   end
-  arr.sort.reverse
+  str = ''
+  arr.sort.reverse.each do |i|
+    str << i
+  end
+  return str
 end
