4f4d90df5c2001d0bbf952eb81ce0ab502f7b3d7
Sutulov <sav.xtx@gmail.com>
2021-06-27 23:26:41 +0500

    after revision

--- Part1_lesson8/homework/check.rb
+++ Part1_lesson8/homework/check.rb
@@ -1,7 +1,6 @@
 print 'Введите аргумент: '
 input = gets.chomp
-#arr = arr.map(&:to_s)
-puts (if (input.to_i == 0 || input.to_f == 0.0) && input != '0' && input != '0.0'
+puts (if input.to_i == 0 && input != '0' && input.to_f.to_s != input
         'Строка'
       elsif input.include? (".")
         'Вещественное число'
