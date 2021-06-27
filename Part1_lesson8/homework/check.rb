9d687afa2835380da0aa6a821cfa76bcd057fa46
Sutulov <sav.xtx@gmail.com>
2021-06-27 23:09:39 +0500

    final

--- Part1_lesson8/homework/check.rb
+++ Part1_lesson8/homework/check.rb
@@ -1,6 +1,6 @@
 print 'Введите аргумент: '
 input = gets.chomp
-arr = arr.map(&:to_s)
+#arr = arr.map(&:to_s)
 puts (if (input.to_i == 0 || input.to_f == 0.0) && input != '0' && input != '0.0'
         'Строка'
       elsif input.include? (".")
