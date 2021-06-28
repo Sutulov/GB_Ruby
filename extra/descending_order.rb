62a2836a1ffc4f5658514030d14f2c15f198f46c
Sutulov <sav.xtx@gmail.com>
2021-06-28 21:56:32 +0500

    final

--- extra/descending_order.rb
+++ extra/descending_order.rb
@@ -1,12 +1,3 @@
 def descending_order(n)
-  n.to_s
-  arr = []
-  (0..n.size-1).each do |i|
-    arr.push(n[i])
-  end
-  str = ''
-  arr.sort.reverse.each do |i|
-    str << i
-  end
-  return str.to_i
+  n.to_s.chars.sort.reverse.join.to_i
 end
