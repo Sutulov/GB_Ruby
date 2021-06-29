f95c776d40d2ab018bc856895ddd64ae95063e9a
Sutulov <sav.xtx@gmail.com>
2021-06-29 17:10:19 +0500

    different way

--- extra/list_filtering.rb
+++ extra/list_filtering.rb
@@ -1,7 +1,8 @@
 # frozen_string_literal: true
 
+# remove all string values from the array
 arr = [1, 'a', 'b', 0, 15]
 def list_f(arr)
-  arr.reject! { |element| element == element.to_s }
+  arr.reject { |x| x.is_a? String }
 end
 p list_f(arr)
