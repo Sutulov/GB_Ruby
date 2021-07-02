a3cb41e82527cc041dd6827978b0ce7bc16c0a88
Sutulov <sav.xtx@gmail.com>
2021-07-02 12:20:17 +0500

    some upgrade

--- Part1_lesson9/homework/colors.rb
+++ Part1_lesson9/homework/colors.rb
@@ -1,11 +1,5 @@
-if (Gem.win_platform?)
-  Encoding.default_external = Encoding.find(Encoding.locale_charmap)
-  Encoding.default_internal = __ENCODING__
-
-  [STDIN, STDOUT].each do |io|
-    io.set_encoding(Encoding.default_external, Encoding.default_internal)
-  end
-end
+require_relative 'lib/encoding'
+Rus.new.encoding
 
 colors = {'красный': 1, 'оранжевый': 2, 'жёлтый': 3, 'зелёный': 4, 'голубой': 5,
 				  'синий': 6, 'фиолетовый': 7}
---
+++ Part1_lesson9/homework/lib/encoding.rb
@@ -0,0 +1,12 @@
+class Rus
+  def encoding
+    if (Gem.win_platform?)
+      Encoding.default_external = Encoding.find(Encoding.locale_charmap)
+      Encoding.default_internal = __ENCODING__
+
+      [STDIN, STDOUT].each do |io|
+        io.set_encoding(Encoding.default_external, Encoding.default_internal)
+      end
+    end
+  end
+end
