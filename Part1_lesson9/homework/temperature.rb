6851629d5309ecd2d69f789b5389580a005e1c41
Sutulov <sav.xtx@gmail.com>
2021-07-02 10:52:10 +0500

    final

---
+++ Part1_lesson9/homework/temperature.rb
@@ -0,0 +1,18 @@
+def cel2far(cel)
+  cel * 1.8 + 32
+end
+
+def far2cel(far)
+  (far - 32) / 1.8
+end
+
+print 'Введите 1 для перевода °F в °C или 2 для перевода °С в °F: '
+choice = gets.to_i
+
+if choice == 1
+  print 'Введите °F: '
+  puts format('%.2f °C', far2cel(gets.to_f))
+else
+  print 'Введите °C: '
+  puts format('%.2f °F', cel2far(gets.to_f))
+end
