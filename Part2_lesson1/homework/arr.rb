543a04018cc43c3b49cf80d111e592101bc301fb
Sutulov <sav.xtx@gmail.com>
2021-07-04 18:45:00 +0500

    preparation

--- Part2_lesson1/homework/arr.rb
+++ Part2_lesson1/homework/arr.rb
@@ -1,2 +1,2 @@
-puts "#{ arr = Array.new(10).map! { |element| element = rand(0..99)} }" 
+puts "#{ arr = Array.new(10).map { |element| element = rand(0..99)} }" 
 puts "max = #{arr.max}, min = #{arr.min}"
--- Part2_lesson1/homework/week.rb
+++ Part2_lesson1/homework/week.rb
@@ -1,1 +1,16 @@
-puts today = Time.new
+require 'date'
+
+
+puts today = Time.now
+puts today.strftime "%w"
+puts today.strftime "%u"
+puts today.strftime "%a"
+puts today.strftime "%W"
+puts today.strftime "%G"
+puts today.strftime "%d.%m.%Y"
+puts today.sunday?
+puts today.wday
+puts today.wday.next
+puts today.yday
+puts (today + 2592000).strftime "%a"
+# week = Array.new(7).map { |i| i =   }
