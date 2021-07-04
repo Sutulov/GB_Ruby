09e384bab20dbf0232909ac83b78c46533cbda08
Sutulov <sav.xtx@gmail.com>
2021-07-04 16:41:07 +0500

    final

--- Part2_lesson1/homework/arr.rb
+++ Part2_lesson1/homework/arr.rb
@@ -1,2 +1,2 @@
-print "#{arr = Array.new(10).map! { |element| element = rand(0..99)} } \n" 
+puts "#{ arr = Array.new(10).map! { |element| element = rand(0..99)} }" 
 puts "max = #{arr.max}, min = #{arr.min}"
