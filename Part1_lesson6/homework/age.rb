c51b01370e7ba41c1af3434e3acce8b78b9bdd57
Sutulov <sav.xtx@gmail.com>
2021-07-04 16:59:42 +0500

    preparation

--- Part1_lesson6/homework/age.rb
+++ Part1_lesson6/homework/age.rb
@@ -3,6 +3,6 @@
 puts 'Введите год, месяц и день вашего рождения ****.**.**:'
 date = gets.chomp
 arr = date.split('.').map(&:to_i)
-age = Time.new.year - arr[0]
+age = Time.new.year - arr.first
 age -= 1 if Time.new.month <= arr[1] && (Time.new.day < arr[2])
 puts "Ваш возраст = #{age}"
---
+++ Part2_lesson1/homework/week.rb
@@ -0,0 +1,1 @@
+puts today = Time.new
