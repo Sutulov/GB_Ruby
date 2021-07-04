2f21aaf9f3322e8512f9e0467389a413372c76d9
Sutulov <sav.xtx@gmail.com>
2021-07-04 20:51:20 +0500

    first version

--- Part2_lesson1/homework/week.rb
+++ Part2_lesson1/homework/week.rb
@@ -1,16 +1,3 @@
-require 'date'
-
-
-puts today = Time.now
-puts today.strftime "%w"
-puts today.strftime "%u"
-puts today.strftime "%a"
-puts today.strftime "%W"
-puts today.strftime "%G"
-puts today.strftime "%d.%m.%Y"
-puts today.sunday?
-puts today.wday
-puts today.wday.next
-puts today.yday
-puts (today + 2592000).strftime "%a"
-# week = Array.new(7).map { |i| i =   }
+today = Time.now
+med = 7 - (today.strftime "%u").to_i
+puts Array.new(7).map! { |day| med += 1; day = (today - (7 - med) * 86400).strftime "%d.%m.%Y"}
