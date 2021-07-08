8152c4460fb5bf77bb87669458d397611f4c6845
Sutulov <sav.xtx@gmail.com>
2021-07-08 13:47:24 +0500

    some test .tap

---
+++ Part2_lesson2/tap_2.rb
@@ -0,0 +1,5 @@
+(1..10)                  .tap {|x| puts "original: #{x}" }
+  .to_a                  .tap {|x| puts "array:    #{x}" }
+  .select {|x| x.even? } .tap {|x| puts "evens:    #{x}" }
+  .map {|x| x*x }        .tap {|x| puts "squares:  #{x}" }
+  
