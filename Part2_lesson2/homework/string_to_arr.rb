6ada9c22b80cfcc047622cdd22d05e85306dbb07
Sutulov <sav.xtx@gmail.com>
2021-07-09 20:37:17 +0500

    new variants

--- Part2_lesson2/homework/string_to_arr.rb
+++ Part2_lesson2/homework/string_to_arr.rb
@@ -1,3 +1,4 @@
 # frozen_string_literal: true
 
 p 'Hello world!'.each_char.to_a
+p 'Hello world!'.scan(/[A-Za-z, \.;:!]+?/)
--- Part2_lesson2/homework/task_6.rb
+++ Part2_lesson2/homework/task_6.rb
@@ -2,5 +2,4 @@
 
 require_relative 'lib/general'
 
-size = MONTHS.map(&:size)
-puts(MONTHS.select { |str| str.size == size.min || str.size == size.max })
+puts(MONTHS.minmax { |str1, str2| str1.size <=> str2.size })
