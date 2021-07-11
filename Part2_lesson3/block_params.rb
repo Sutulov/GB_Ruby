bfa849e38680905c26e5d9550922b7cdb56dcf40
Sutulov <sav.xtx@gmail.com>
2021-07-11 10:58:20 +0500

    test

--- Part2_lesson3/block_params.rb
+++ Part2_lesson3/block_params.rb
@@ -1,7 +1,14 @@
 def greeting
-  yield 'Hello', 'Ruby'
+  yield 'Hello', 'Ruby', '!'
 end
 
 greeting do |interjection, noun|
   puts "#{interjection}, #{noun}!"
+end
+
+greeting do |fst, snd, thd, fth|
+  p fst
+  p snd
+  p thd
+  p fst
 end
