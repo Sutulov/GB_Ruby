87da1f1afe4d22c7ad5718f6f3760a114250dae0
Sutulov <sav.xtx@gmail.com>
2021-07-13 21:48:42 +0500

    first version

--- Part2_lesson3/homework/week.rb
+++ Part2_lesson3/homework/week.rb
@@ -1,12 +1,12 @@
-today = Time.now
+DAY = 86_400
+
 def week(n)
-  yield n
+  today = Time.now
+  p (today.strftime '%W').to_i
+  med = (today.strftime '%j').to_i - n * 7 + 4
+  # yield n
+  yield Array.new(7).map { (today - (med -= 1) * DAY).strftime '%d.%m.%Y' }
 end
 
 print 'Введите номер недели: '
 week(gets.to_i) { |num| puts num }
-
-# p today.strftime '%W'
-# med = 7 - (today.strftime '%u').to_i
-
-# puts (Array.new(7).map { (today - (7 - (med += 1)) * 86_400).strftime '%d.%m.%Y' })
