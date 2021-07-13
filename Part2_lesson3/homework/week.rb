19b5dbf66a793980e2c936bb394a175bf4024a59
Sutulov <sav.xtx@gmail.com>
2021-07-13 21:50:26 +0500

    some corrections

--- Part2_lesson3/homework/week.rb
+++ Part2_lesson3/homework/week.rb
@@ -2,9 +2,7 @@ DAY = 86_400
 
 def week(n)
   today = Time.now
-  p (today.strftime '%W').to_i
   med = (today.strftime '%j').to_i - n * 7 + 4
-  # yield n
   yield Array.new(7).map { (today - (med -= 1) * DAY).strftime '%d.%m.%Y' }
 end
 
