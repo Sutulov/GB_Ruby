ca32390bfead05d73cf24ee8c4dbe2950e974eae
Sutulov <sav.xtx@gmail.com>
2021-07-15 00:29:06 +0400

    first version

---
+++ Part2_lesson3/homework/fibonacci.rb
@@ -0,0 +1,8 @@
+def fibonacci(n)
+  (1..n).map do |num|
+    yield (((((1 + 5**0.5) / 2)**num) - (((1 - 5**0.5) / 2)**num)) / 5**0.5).to_i
+  end
+end
+
+print 'Введите число: '
+fibonacci(gets.to_i) { |f| print "#{f} " }
