69f1d52af2236044f385a49b21b20a20e8bfb6a0
Sutulov <sav.xtx@gmail.com>
2021-07-15 00:09:51 +0400

    {}

--- Part2_lesson3/homework/weekends.rb
+++ Part2_lesson3/homework/weekends.rb
@@ -3,8 +3,9 @@ require 'date'
 def weekends
   day = Date.new(2021,1,1)
   while day != Date.new(2022,1,1) do
-    puts day.strftime('%a %d %b %Y') if day.saturday? || day.sunday?
+    yield day.strftime('%a %d %b %Y') if day.saturday? || day.sunday?
     day += 1
   end
 end
-weekends
+
+weekends { |day| puts day }
