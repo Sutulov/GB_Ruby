6f802e2f7899734975ac905bb2bd105afd40e8f4
Sutulov <sav.xtx@gmail.com>
2021-07-15 16:24:04 +0400

    first version select

--- Part2_lesson3/homework/lib/new_methods.rb
+++ Part2_lesson3/homework/lib/new_methods.rb
@@ -8,16 +8,22 @@ end
 
 class Array
   def my_select
+    new = []
     self.each do |i|
-      yield i
+      new << i if yield i
     end
+    new
   end
 end
 
 class Array
   def my_reduce
+    m = self.first
     self.each do |i|
-      yield i
+      yield begin
+        m += i 
+        i + 5
+      end
     end
   end
 end
--- Part2_lesson3/homework/my_reduce.rb
+++ Part2_lesson3/homework/my_reduce.rb
@@ -1,6 +1,9 @@
 require_relative 'lib/new_methods'
 
 old = [1, 2, 3, 4, 5]
-new = old.my_select { |i| i.even? }
+new = old.reduce do |m, i| 
+  puts i
+  puts m
+end
 # puts old
-p new
+# puts new
