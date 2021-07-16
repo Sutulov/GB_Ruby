4040b37d7bae710ee1221fee583eb97feb326b82
Sutulov <sav.xtx@gmail.com>
2021-07-16 22:27:37 +0400

    first version

--- Part2_lesson4/homework/factory_use.rb
+++ Part2_lesson4/homework/factory_use.rb
@@ -1,2 +1,11 @@
 require_relative 'lib/factory'
 
+Factory.build(:ball)
+Factory.build(:ball)
+Factory.build(:ball)
+Factory.build(:cube)
+Factory.build(:teddy_bear)
+Factory.build(:teddy_bear)
+
+puts "Количество созданных игрушек: #{Factory.total}"
+puts Factory.offers
--- Part2_lesson4/homework/lib/factory.rb
+++ Part2_lesson4/homework/lib/factory.rb
@@ -1,13 +1,43 @@
 class Factory
-  def build
+  @@counter_teddy_bear = 0
+  @@counter_ball = 0
+  @@counter_cube = 0
+  @@counter_toys = 0
+
+  def self.build(toy)
+    @@counter_toys += 1
+    case toy
+    when toy = :teddy_bear
+      @teddy_bear = TeddyBear.new
+      @@counter_teddy_bear += 1
+    when toy = :ball
+      @ball = Ball.new
+      @@counter_ball += 1
+    when toy = :cube
+      @cube = Cube.new
+      @@counter_cube += 1
+    end
+  end
+
+  def self.total
+    @@counter_toys
+  end
+
+  def self.offers
+    hash ={teddy_bear: @@counter_teddy_bear, ball: @@counter_ball, cube: @@counter_cube}
+  end
+
+  class TeddyBear
 
   end
 
-  def total
+  class Ball
 
   end
 
-  def offers
+  class Cube
 
   end
+
+
 end
