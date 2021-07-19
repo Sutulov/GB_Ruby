1f0791ed97f183b52ea3ca1a47a5551f2ba9a310
Sutulov <sav.xtx@gmail.com>
2021-07-19 17:55:14 +0400

    test

--- Part2_lesson5/ticket_param.rb
+++ Part2_lesson5/ticket_param.rb
@@ -1,7 +1,11 @@
 require_relative 'lib/ticket'
 
-first = Ticket.new
-second = Ticket.new(600)
+first = Ticket.new(date: Time.mktime(2021, 7, 21, 10, 20))
 
+puts "Дата билета first: #{first.date}"
 puts "Цена билета first: #{first.price}"
+
+second = Ticket.new(date: Time.mktime(2021, 7, 22, 10, 20), price: 600)
+
+puts "Дата билета second: #{second.date}"
 puts "Цена билета second: #{second.price}"
