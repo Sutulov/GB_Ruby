0d979fef195baf7d8ced74e1720e057eebad9235
Sutulov <sav.xtx@gmail.com>
2021-07-20 16:06:04 +0400

    test

---
+++ Part2_lesson5/ticket_setter.rb
@@ -0,0 +1,22 @@
+class Ticket
+  def initialize(date:, price: 500)
+    @price = price
+    @date = date
+  end
+
+  def price=(price)
+    @price = price
+  end
+
+  def price
+    @price
+  end
+
+  def date=(date)
+    @date = date
+  end
+
+  def date
+    @date
+  end
+end
---
+++ Part2_lesson5/ticket_setter_use.rb
@@ -0,0 +1,12 @@
+require_relative 'ticket_setter'
+
+ticket = Ticket.new(
+           date: Time.new(2021, 7, 21, 10, 20),
+           price: 500
+       )
+
+ticket.price = 600
+ticket.date = Time.new(2021, 7, 22, 10, 20)
+
+puts "#{ticket.price}"
+puts "#{ticket.date}"
