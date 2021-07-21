69f9b5028d363cdedd82e0c28745f91dff1ae91b
Sutulov <sav.xtx@gmail.com>
2021-07-21 19:54:11 +0400

    preparation

--- Part2_lesson6/homework/task2.rb
+++ Part2_lesson6/homework/task2.rb
@@ -1,3 +1,3 @@
 hash = Hash.new
-%w[first second third].each_with_index { |key, num| hash[key.to_sym] = key[0].capitalize + key[-2, 2] + "(#{num})" }
-p hash
+%w[first second third].each_with_index { |key, num| hash[key.to_sym] = key[0].capitalize + key[-2, 2] + "(#{num + 1})" }
+puts hash
