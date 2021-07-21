b4adc4b0518503a64982645615a64e90edc74816
Sutulov <sav.xtx@gmail.com>
2021-07-21 22:59:47 +0400

    preparation

---
+++ Part2_lesson6/homework/task5.rb
@@ -0,0 +1,2 @@
+ENG = %w[red orange yellow green gray indigo violet]
+RUS = %w[красный оранжевый желтый зеленый голубой синий фиолетовый]
---
+++ Part2_lesson6/homework/task6.rb
@@ -0,0 +1,3 @@
+NUM = 100
+p format("%h" % NUM)
+p format("%x" % NUM)
