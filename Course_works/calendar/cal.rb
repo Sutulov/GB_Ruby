c8696311a4b0e77769362d632be0925d04085bae
Sutulov <sav.xtx@gmail.com>
2021-07-23 03:07:07 +0400

    preparation

---
+++ Course_works/calendar/cal.rb
@@ -0,0 +1,30 @@
+DAY = 86_400
+today =  Time.now
+puts today.strftime '    %B %Y'
+n = (Time.now.strftime '%e').to_i
+day = today - n * DAY
+month = Array.new(7, []) 
+week = %w[пн вт ср чт пт сб вс]
+month = week.zip(month)
+
+(1..31).each do |i|
+   day += DAY
+  case day.strftime '%u'
+  when '1'
+    month.first << ' ' + day.day.to_s
+  when '2'
+    month[1] << ' ' + day.day.to_s
+  when '3'
+    month[2] << ' ' + day.day.to_s
+  when '4'
+    month[3] << ' ' + day.day.to_s
+  when '5'
+    month[4] << ' ' + day.day.to_s
+  when '6'
+    month[5] << ' ' + day.day.to_s
+  when '7'
+    month.last << ' ' + day.day.to_s
+  end
+end
+
+month.map { |arr| puts arr.join }
