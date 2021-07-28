c12a1d715b55b14b6de11e6d6abbb31636f70016
Sutulov <sav.xtx@gmail.com>
2021-07-28 15:21:46 +0400

    final version

--- Course_works/vocabulary/lib/search.rb
+++ Course_works/vocabulary/lib/search.rb
@@ -6,7 +6,7 @@ class Search
     open('lib/vocabulary.txt') do |f|
       @word2 = f.read.split.select { |str| str.start_with?(word.upcase) }
     end
-      arr = @word2.each { |str| return str if str == word.upcase }
-    arr.empty? ? 'Нет такого слова' : arr
+    @word2.each { |str| return str if str == word.upcase }
+    @word2.empty? ? 'Нет такого слова' : @word2
   end
 end
