60ebecc87851676d85be9157e9cf14f732c26973
Sutulov <sav.xtx@gmail.com>
2021-07-28 15:25:33 +0400

    change name var

--- Course_works/vocabulary/lib/search.rb
+++ Course_works/vocabulary/lib/search.rb
@@ -4,9 +4,9 @@
 class Search
   def self.words(word)
     open('lib/vocabulary.txt') do |f|
-      @word2 = f.read.split.select { |str| str.start_with?(word.upcase) }
+      @arr = f.read.split.select { |str| str.start_with?(word.upcase) }
     end
-    @word2.each { |str| return str if str == word.upcase }
-    @word2.empty? ? 'Нет такого слова' : @word2
+    @arr.each { |str| return str if str == word.upcase }
+    @arr.empty? ? 'Нет такого слова' : @arr
   end
 end
