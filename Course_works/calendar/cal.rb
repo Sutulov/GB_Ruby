5b81300f07da1ce26b131043eceebafeca0cb657
Sutulov <sav.xtx@gmail.com>
2021-07-28 10:04:57 +0400

    highlighted the current date

--- Course_works/calendar/cal.rb
+++ Course_works/calendar/cal.rb
@@ -4,11 +4,15 @@ require_relative 'lib/data'
 
 today = Time.now
 
-puts "    #{MONTHS[(today.strftime '%b').to_sym]} #{today.year}"
+puts "     #{MONTHS[(today.strftime '%b').to_sym]} #{today.year}"
 month = Month.arr_month(today)
 month.map do |arr|
   arr.map do |str|
-    print format('%3s', str)
+    if str == " #{today.day}"
+      print format("\e[31m%3s\e[0m", str)
+    else
+      print format('%3s', str)
+    end
   end
   puts
 end
