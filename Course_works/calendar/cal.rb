107b9b0705eaf54f92e2ac6a6145309f047ee23f
Sutulov <sav.xtx@gmail.com>
2021-07-28 09:15:33 +0400

    add color

--- Course_works/calendar/cal.rb
+++ Course_works/calendar/cal.rb
@@ -6,4 +6,4 @@ today = Time.now
 
 puts "    #{MONTHS[(today.strftime '%b').to_sym]} #{today.year}"
 month = Month.arr_month(today)
-month.map { |arr| puts format("% 3s\% 3s\% 3s\% 3s\% 3s\% 3s" % arr) }
+month.map { |arr| puts format("% 3s\e[32m% 3s\% 3s\% 3s\% 3s\% 3s\e[0m" % arr) }
