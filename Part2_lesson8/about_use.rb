8cc36cd3bb5b06a2f8a4abcbc686855d1215f0f8
Sutulov <sav.xtx@gmail.com>
2021-07-29 12:00:18 +0400

    test

---
+++ Part2_lesson8/about_use.rb
@@ -0,0 +1,12 @@
+%w[about news photo_catalog].each do |file|
+  require_relative "lib/#{file}"
+end
+
+about = About.new
+
+about.title = 'О нас'
+about.body = 'Вы можете обнаружить нас по адресу'
+about.keywords = ['О нас', 'Адреса', 'Телефоны']
+about.phones = ['+7 920 4567731', '+7 920 4563123']
+
+p about
