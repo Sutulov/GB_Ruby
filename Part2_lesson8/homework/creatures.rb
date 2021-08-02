d07278366537315fbe79730b08d39c669395c7d2
Sutulov <sav.xtx@gmail.com>
2021-08-02 12:14:23 +0500

    preparation

---
+++ Part2_lesson8/homework/creatures.rb
@@ -0,0 +1,5 @@
+require_relative 'lib/evolution'
+
+puts n = Animals.new
+puts x = People.new
+p arr = Array.new(7, *HomoSapiens.ancestors)
---
+++ Part2_lesson8/homework/lib/evolution.rb
@@ -0,0 +1,20 @@
+class Animals
+end
+
+class Chordates < Animals
+end
+
+class Mammals < Chordates
+end
+
+class Primates < Mammals
+end
+
+class Hominids < Primates
+end
+
+class People < Hominids
+end
+
+class HomoSapiens < People
+end
