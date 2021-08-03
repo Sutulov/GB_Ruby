209277e2d5ab988ef02c22867ca33e8ecb8c8d3f
Sutulov <sav.xtx@gmail.com>
2021-08-03 12:54:57 +0500

    test

---
+++ Part2_lesson9/ticket_child.rb
@@ -0,0 +1,20 @@
+require_relative 'lib/ticket'
+
+class TicketChild < Ticket
+  def price
+    price_format(@price)
+  end
+
+  def number
+    super
+  end
+end
+
+first = TicketChild.new(date: Time.mktime(2021, 8, 7, 10, 20))
+second = TicketChild.new(date: Time.mktime(2021, 8, 7, 10, 20))
+
+puts first <=> second
+
+puts second.price
+puts second.number
+puts second.price_format(600)
