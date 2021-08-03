8caff6d13f90705873f7768f0fc4aab39c05d736
Sutulov <sav.xtx@gmail.com>
2021-08-03 10:57:49 +0500

    test

---
+++ Part2_lesson9/singleton.rb
@@ -0,0 +1,23 @@
+class Singleton
+  def self.instance
+    @@obj ||= new
+  end
+
+  def dup
+    @@obj
+  end
+
+  private_class_method :new
+  alias clone dup
+end
+
+first = Singleton.instance
+p first
+second = Singleton.instance
+p second
+
+p first.dup
+
+p first.clone
+
+third = Singleton.new
