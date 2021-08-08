aa2f5755865401d3a6392673adb594eb7f0adc41
Sutulov <sav.xtx@gmail.com>
2021-08-08 21:29:57 +0400

    test

---
+++ Part3/lesson1/extname.rb
@@ -0,0 +1,8 @@
+path = File.join('.')
+entries = Dir.new(path).entries.reject { |x| %w[. ..].include? x }
+
+counter = 0
+entries.each do |item|
+  counter += 1 if File.extname(item) == '.rb'
+end
+p counter
---
+++ Part3/lesson1/scan.rb
@@ -0,0 +1,2 @@
+path = File.join('.')
+p Dir.new(path).entries.reject { |x| %w[. ..].include? x }
