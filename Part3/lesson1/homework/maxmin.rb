9c52de195eaf98d9d3254ba60c13153f2c3c0fc8
Sutulov <sav.xtx@gmail.com>
2021-08-11 19:56:12 +0400

    some correction

--- Part3/lesson1/homework/maxmin.rb
+++ Part3/lesson1/homework/maxmin.rb
@@ -1,5 +1,5 @@
 # frozen_string_literal: true
 
 arr = File.open(ARGV.first, 'r').readlines.minmax_by(&:size)
-print arr[1] + "Количество символов:  #{arr[1].chomp.size}\n"
+puts arr[1] + "Количество символов:  #{arr[1].chomp.size}"
 puts arr.first + "Количество символов:  #{arr.first.chomp.size}"
