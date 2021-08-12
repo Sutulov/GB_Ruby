e45d4b83d959ba180b471a4464437cf0c68658b6
Sutulov <sav.xtx@gmail.com>
2021-08-12 21:49:05 +0400

    after rubocop

--- Part3/lesson2/homework/lib/size.rb
+++ Part3/lesson2/homework/lib/size.rb
@@ -1,14 +1,16 @@
 # frozen_string_literal: true
+
 KILO = 1024.0
 
+# Module suze for files
 module Size
   def self.performance(size)
     if size > KILO && size < KILO * KILO
       "#{(size / KILO).round(1)} K"
-    elsif size > KILO ** 2 && size < KILO ** 3
-      "#{(size / (KILO ** 2)).round(1)} M"
-    elsif size > KILO ** 3
-      "#{(size / (KILO ** 3)).round(1)} G"
+    elsif size > KILO**2 && size < KILO**3
+      "#{(size / (KILO**2)).round(1)} M"
+    elsif size > KILO**3
+      "#{(size / (KILO**3)).round(1)} G"
     else
       "#{size.round} bytes"
     end
