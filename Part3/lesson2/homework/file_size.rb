1ded7e62cc7c8e9a6c1b59be432f4a6930e3aa3c
Sutulov <sav.xtx@gmail.com>
2021-08-12 21:13:09 +0400

    first version

--- Part3/lesson2/homework/file_size.rb
+++ Part3/lesson2/homework/file_size.rb
@@ -1,2 +1,13 @@
+KILO = 1024
+
 file = File.open(ARGV.first)
 p file.size
+if file.size > KILO && file.size < KILO * KILO
+  puts "#{file.size / 1024} K"
+elsif file.size > KILO ** 2 && file.size < KILO ** 3
+  puts "#{file.size / (KILO ** 2)} M"
+elsif file.size > KILO ** 3
+  puts "#{file.size / (KILO ** 3)} G"
+else
+  puts "#{file.size} bytes"
+end
