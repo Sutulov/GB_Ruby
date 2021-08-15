f0992fdee1af57a40513a731b203501afe3f0fd2
Sutulov <sav.xtx@gmail.com>
2021-08-16 01:14:26 +0400

    first version

--- Part3/lesson3/homework/lib/seo.rb
+++ Part3/lesson3/homework/lib/seo.rb
@@ -1,3 +1,19 @@
 class Seo
-  attr_accessor :title, :description, :keywords
+  class << self
+    def title=(title)
+      @title = title
+    end
+
+    def description=(description)
+      @description = description
+    end
+
+    def keywords=(keywords)
+      @keywords = keywords
+    end
+
+    def data
+      [@title, @description, @keywords]      
+    end
+  end
 end
--- Part3/lesson3/homework/use_seo.rb
+++ Part3/lesson3/homework/use_seo.rb
@@ -2,8 +2,27 @@ require_relative 'lib/seo'
 
 class News
   attr_accessor :date
+  
+  def initialize
+    @data = Seo.data
+  end
 end
 
 class About
   attr_accessor :phones
+
+  def initialize
+    @data = Seo.data
+  end
 end
+
+
+Seo.title = 'О нас'
+Seo.description = 'Адрес: Самара'
+Seo.keywords = ['О нас', 'Адреса', 'Телефоны']
+
+
+news = News.new
+p news
+about = About.new
+p about
