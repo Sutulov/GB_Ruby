a43cc0eed9eb996107554c2628bfef0274c29d02
Sutulov <sav.xtx@gmail.com>
2021-08-19 01:20:58 +0400

    use Forwardable

---
+++ Part3/lesson4/decorator.rb
@@ -0,0 +1,39 @@
+require 'singleton'
+require 'forwardable'
+
+module Decorator
+  def self.included(cls)
+    cls.extend Forwardable
+    cls.extend ClassMethods
+  end
+  module ClassMethods
+    def decorate(*methods, to:, as: nil)
+      if as
+        def_delegator to, methods.first, as
+      else
+        def_delegators to, *methods
+      end
+    end
+  end
+end
+
+class Settings
+  include Singleton
+  include Decorator
+  decorate :[]=,  :[], to: :@list
+  decorate :join, to: '@list.map { |k, v| "#{k} #{v} " }', as: :report
+
+  def initialize
+    @list = {}
+  end
+end
+
+setting = Settings.instance
+setting[:title] = 'Новостной портал'
+setting[:per_page] = 30
+
+params = Settings.instance
+p params[:title]
+p params[:per_page]
+
+puts params.report
