0c810735ae1d87976872c003cf2e625b9b699701
Sutulov <sav.xtx@gmail.com>
2021-08-19 20:22:53 +0400

    preparation

--- Part3/lesson4/homework/lib/group.rb
+++ Part3/lesson4/homework/lib/group.rb
@@ -1,6 +1,25 @@
 class Group
   include Enumerable
+  attr_accessor :users
+
+  def initialize(surname, name, position)
+    User.new(surname, name, position).user { |fio, position| @users[[fio] = position }
+
+
+  def each
+    users.each { |user| yield user }
+  end
+
 
   class User
+    attr_accessor :fio, position
+
+    def initialize(fio, position)
+      @fio = fio
+      @position = position
+    end
+
+    def user
+      yield fio, position
   end
 end
