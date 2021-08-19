2acc4ee210738faeeff5f48fb1140818e2ad02ff
Sutulov <sav.xtx@gmail.com>
2021-08-20 00:56:44 +0400

    final version

--- Part3/lesson4/homework/add_to_json.rb
+++ Part3/lesson4/homework/add_to_json.rb
@@ -2,9 +2,23 @@
 class Hash
   def to_json(*_args)
     json = '{'
-    each { |key, value| json << "\"#{key}\":\"#{value}\"," }
-    json.delete_suffix!(",")
+    each do |key, value|
+      json << "\"#{key}\":"
+      json << values(value)
+    end
+    json.delete_suffix!(',')
     json << '}'
+  end
+
+  def values(value)
+    case value
+    when Hash
+      value.to_json
+    when String
+      "\"#{value}\","
+    else
+      "#{value},"
+    end
   end
 end
 
@@ -16,4 +30,17 @@ hash = {
   'Крыгин Пётр Алексеевич' => 'тестировщик',
   'Иванова Анна Серьгеевна' => 'дизайнер'
 }
+
 puts hash.to_json
+
+person = {
+  first_name: 'Иван',
+  last_name: 'Петрович',
+  patronymic: 'Сидоров',
+  params: {
+    score: 20,
+    number: 762
+  }
+}
+
+puts person.to_json
