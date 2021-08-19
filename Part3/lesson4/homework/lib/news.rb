20a330151e6baf4b04bf4d276160934ee7693aba
Sutulov <sav.xtx@gmail.com>
2021-08-19 20:52:54 +0400

    preparation

---
+++ Part3/lesson4/homework/lib/news.rb
@@ -0,0 +1,14 @@
+class News
+  include Comparable
+  attr_accessor :title, :body, :date
+
+  def initialize(title, body, date)
+    @title = title
+    @body = body
+    @date = date
+  end
+
+  def <=>(news)
+    date <=> news.date
+  end
+end
