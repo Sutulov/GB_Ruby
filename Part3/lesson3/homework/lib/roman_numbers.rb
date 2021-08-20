8f9dc5aa21f82dfa1c334fa0d2afe25b07eb2c75
Sutulov <sav.xtx@gmail.com>
2021-08-20 23:33:52 +0400

    some corrections

--- Part3/lesson3/homework/lib/roman_numbers.rb
+++ Part3/lesson3/homework/lib/roman_numbers.rb
@@ -1,19 +1,15 @@
-# frozen_string_literal: true
-
 # Module convert arabic to roman numbers
 module RomanNumbers
   ROM = %w[M CM D CD C XC L XL X IX V IV I].freeze
   ARB = [1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1].freeze
 
   def roman
-    result = ''
     arb = self
-    ARB.each_with_index do |num, i|
+    ARB.each_with_index.each_with_object('') do |(num, i), result|
       while arb >= num
         arb -= num
-        result += ROM[i]
+        result << ROM[i]
       end
     end
-    result
   end
 end
