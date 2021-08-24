497ebf7c17804e6c7d938343c6f34c75a3cf316e
Sutulov <sav.xtx@gmail.com>
2021-08-24 21:01:16 +0400

    test

--- Part3/lesson5/homework.rb/cube_use.rb
+++ Part3/lesson5/homework.rb/cube_use.rb
@@ -1,1 +1,4 @@
 require_relative 'lib/cube'
+
+cube = Cube.new([0, 0, 0], [0, 0, 1], [0, 1, 0], [1, 0, 0])
+p cube
--- Part3/lesson5/homework.rb/lib/cube.rb
+++ Part3/lesson5/homework.rb/lib/cube.rb
@@ -1,2 +1,14 @@
 class Cube
+  attr_accessor :cubes
+
+  def initialize(cube)
+    Item.new(cube)
+  end
+
+  class Item
+    attr_accessor :coordinates
+
+    def initialize(coordinates)
+      @coordinates = coordinates      
+    end
 end
