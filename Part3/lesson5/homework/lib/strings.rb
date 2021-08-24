ad841515d336f9b4a9388a5612f88c14d9289b02
Sutulov <sav.xtx@gmail.com>
2021-08-24 22:57:55 +0400

    test

--- Part3/lesson5/homework/lib/strings.rb
+++ Part3/lesson5/homework/lib/strings.rb
@@ -2,8 +2,15 @@ module Str
   attr_accessor :lines
   class << self
     def input
-      @lines = ''
-      (0..3).each { @lines << gets }
+      @lines = (0..2).each_with_object([]) { |i, lines| lines << check(gets.chomp) }
+    end
+
+    def check(line)
+      if String === line 
+        line
+      else
+        raise 'Цифры, пробелы, знаки препинания — не допускаются!'
+      end
     end
 
     def output
