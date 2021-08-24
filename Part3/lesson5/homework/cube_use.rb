16c38263aef3f5a4c84713bc8bdea8b26e4d960e
Sutulov <sav.xtx@gmail.com>
2021-08-24 21:52:17 +0400

    first version

--- Part3/lesson5/homework/cube_use.rb
+++ Part3/lesson5/homework/cube_use.rb
@@ -1,4 +1,10 @@
 require_relative 'lib/cube'
 
-cube = Cube.new([0, 0, 0], [0, 0, 1], [0, 1, 0], [1, 0, 0])
-p cube
+cube = Cube.new([[0, 0, 0], [0, 0, 1], [0, 1, 0], [1, 0, 0]])
+p cube.cube
+cube2 = Cube.new([[1, 1, 1], [1, 1, 2], [1, 2, 1], [2, 1, 1]])
+p cube2.cube
+cube3 = Cube.new([[0, 0, 0], [0, 0, 1], [0, 1, 0], [1, 0, 0]])
+p cube3.cubes
+cube4 = Cube.new([[0, 0, 0], [0, 0, 3], [0, 3, 0], [3, 0, 0]])
+p cube4.cube
--- Part3/lesson5/homework/lib/cube.rb
+++ Part3/lesson5/homework/lib/cube.rb
@@ -1,14 +1,25 @@
+# class Cube 
 class Cube
-  attr_accessor :cubes
+  attr_accessor :cubes, :cube
+  @@cubes = {}
 
-  def initialize(cube)
-    Item.new(cube)
+  def initialize(coordinates)
+    raise 'Уже есть такой куб!' if @@cubes.key?(coordinates)
+    @cube = coordinates
+    Item.new(coordinates).cube { |coordinates| @@cubes[coordinates] = 1 }
   end
 
+  
+  # Class Item
   class Item
     attr_accessor :coordinates
 
     def initialize(coordinates)
-      @coordinates = coordinates      
+      @coordinates = coordinates
     end
+
+    def cube
+      yield coordinates
+    end
+  end
 end
