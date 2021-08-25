6c194be45a504d6d56dc8929be6a957bea3cb83e
Sutulov <sav.xtx@gmail.com>
2021-08-25 21:18:33 +0400

    test

--- Part3/lesson6/homework/task02.rb
+++ Part3/lesson6/homework/task02.rb
@@ -1,8 +1,16 @@
 matrix = [
-[1, 2, 3],
-[4, 5, 6],
-[7, 8, 9],
+[1, 2, 3, 4],
+[4, 5, 6, 5],
+[7, 8, 9, 7],
+[7, 5, 6, 7]
 ]
-puts  matrix
+p  matrix
+p matrix.size
+size = matrix.flatten.size
+if size.even?
+  num = matrix.flatten[size / 2 - 1].to_i
+else
+  num = matrix.flatten[size / 2].to_i
+end
 matrix.flatten.size
-puts matrix.flatten.first.to_i + matrix.flatten.last.to_i
+puts matrix.flatten.first.to_i + matrix.flatten.last.to_i + num
