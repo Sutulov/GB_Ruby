44d24de2fe27696d86ba9d6257d72d3fbfd2f475
Sutulov <sav.xtx@gmail.com>
2021-08-25 10:07:17 +0500

    after rubocop

--- Part3/lesson5/homework/dirs.rb
+++ Part3/lesson5/homework/dirs.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 # module to define all directories
 module Dirs
   def scan(path)
@@ -6,7 +8,7 @@ module Dirs
                  .reject { |x| %w[. ..].include? x }
                  .map { |x| File.join(path, x) }
 
-    entries.reduce('') do |dir, item|
+    entries.each do |item|
       if File.directory?(item)
         scan(item)
         puts item
