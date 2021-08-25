d6da2815d134aa53b829cc1f007576649d8b8924
Sutulov <sav.xtx@gmail.com>
2021-08-25 08:52:41 +0500

    after rubocop

--- Part3/lesson5/homework/lib/strings.rb
+++ Part3/lesson5/homework/lib/strings.rb
@@ -1,19 +1,21 @@
+# frozen_string_literal: true
+
+# module working with input strings
 module Str
   attr_accessor :lines
 
   class << self
     def input
-      @lines = (0..3).each_with_object({}) do |i, lines| 
-                                            if lines.key?(str = check(gets.chomp))
-                                              lines[str] += 1
-                                            else
-                                              lines[str] = 1
-                                            end
-                                          end
+      @lines = (0..3).each_with_object({}) do |_i, lines|
+        if lines.key?(str = check(gets.chomp))
+          lines[str] += 1
+        else
+          lines[str] = 1
+        end
+      end
     end
 
     def check(line)
-
       if line == line.to_i.to_s || line.scan(/[, .;:!]+?/) != []
         raise 'Цифры, пробелы, знаки препинания — не допускаются!'
       else
@@ -22,13 +24,13 @@ module Str
     end
 
     def output
-      @lines.sort.each do |key, value| 
-                         if value > 1
-                           puts "#{key}(#{value})"
-                         else
-                          puts key
-                         end
-                       end
+      @lines.sort.each do |key, value|
+        if value > 1
+          puts "#{key}(#{value})"
+        else
+          puts key
+        end
+      end
     end
   end
 end
