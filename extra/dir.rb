32322c86df270649f2128b9a9f735131b1482df5
Sutulov <sav.xtx@gmail.com>
2021-08-25 10:54:43 +0500

    alternative version

---
+++ extra/dir.rb
@@ -0,0 +1,5 @@
+dir_size = Dir.glob('**/*').select { |path| File.file?(path) }
+              .reduce(0) { |size, path| size + File.size(path) }
+p dir_size
+# p Dir.glob('**/**')
+puts Dir["./**/"]
