90303e1367c224b7f991c3b03e70ebf2900ed86e
Sutulov <sav.xtx@gmail.com>
2021-08-25 10:03:47 +0500

    first version

--- Part3/lesson5/homework/dirs.rb
+++ Part3/lesson5/homework/dirs.rb
@@ -1,9 +1,22 @@
+# module to define all directories
 module Dirs
-  def input
-    Dir.glob('**')
+  def scan(path)
+    entries = Dir.new(path)
+                 .entries
+                 .reject { |x| %w[. ..].include? x }
+                 .map { |x| File.join(path, x) }
+
+    entries.reduce('') do |dir, item|
+      if File.directory?(item)
+        scan(item)
+        puts item
+      end
+    end
   end
 end
 
 include Dirs
 
-puts Dirs.input
+path = File.join('.')
+
+scan(path)
