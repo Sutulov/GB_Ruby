9d38fa40924d38ba6ead44f89e717d2570ee5b0f
Sutulov <sav.xtx@gmail.com>
2021-08-26 10:56:04 +0500

    first version

--- Part3/lesson6/homework/task02.rb
+++ Part3/lesson6/homework/task02.rb
@@ -4,13 +4,7 @@ matrix = [
 [7, 8, 9, 7],
 [7, 5, 6, 7]
 ]
-p  matrix
-p matrix.size
-size = matrix.flatten.size
-if size.even?
-  num = matrix.flatten[size / 2 - 1].to_i
-else
-  num = matrix.flatten[size / 2].to_i
-end
-matrix.flatten.size
-puts matrix.flatten.first.to_i + matrix.flatten.last.to_i + num
+p matrix
+sum = 0
+matrix.each_with_index {  |item, i| sum += item[i] }
+puts sum
