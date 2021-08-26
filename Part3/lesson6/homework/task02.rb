9e4fa15f14861e6f99d5168da9416a9ffd322bb1
Sutulov <sav.xtx@gmail.com>
2021-08-26 20:20:15 +0400

    some corrections

--- Part3/lesson6/homework/task02.rb
+++ Part3/lesson6/homework/task02.rb
@@ -1,12 +1,11 @@
 # frozen_string_literal: true
 
-matrix = [
+MATRIX = [
   [1, 2, 3, 4],
   [4, 5, 6, 5],
   [7, 8, 9, 7],
   [7, 5, 6, 7]
-]
-p matrix
-trace = 0
-matrix.each_with_index { |item, i| trace += item[i] }
-puts trace
+].freeze
+
+p MATRIX
+puts MATRIX.each_with_index.reduce(0) { |trace, (item, i)| trace + item[i] }
