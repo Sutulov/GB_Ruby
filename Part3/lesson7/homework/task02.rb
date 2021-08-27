a87b1bcf4daf1581c4805e11476830d54e9d70e2
Sutulov <sav.xtx@gmail.com>
2021-08-27 21:42:24 +0400

    first version

---
+++ Part3/lesson7/homework/task02.rb
@@ -0,0 +1,12 @@
+colors = {
+  red: 'красный',
+  orange: 'оранжевый',
+  yellow: 'желтый',
+  green: 'зеленый',
+  blue: 'голубой',
+  indigo: 'синий',
+  violet: 'фиолетовый'
+}
+p rus = colors.values
+p eng = colors.keys
+p new = rus.zip(eng).to_h
