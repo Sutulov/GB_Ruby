8e3be3c67c3f9fb6bb57dd00102b7205fb7d166d
Sutulov <sav.xtx@gmail.com>
2021-08-27 21:37:47 +0400

    after rubocop

--- Part3/lesson7/homework/task01.rb
+++ Part3/lesson7/homework/task01.rb
@@ -1,8 +1,11 @@
-countries = {
+# frozen_string_literal: true
+
+COUNTRIES = {
   Россия: 145_912_025,
   Турция: 85_191_618,
   Германия: 84_035_386,
   Франция: 67_076_000,
   Великобритания: 66_796_807
-}
-p countries
+}.freeze
+
+p COUNTRIES
