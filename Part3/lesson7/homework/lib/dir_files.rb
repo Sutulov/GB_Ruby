2c02638128831ba85bb93d84fe24e57569f1636c
Sutulov <sav.xtx@gmail.com>
2021-08-28 12:20:30 +0500

    first version

---
+++ Part3/lesson7/homework/lib/dir_files.rb
@@ -0,0 +1,23 @@
+# module to define all directories
+module DirFiles
+  def scan(folder)
+    entries = Dir.new(folder)
+                 .entries
+                 .reject { |x| %w[. ..].include? x }
+                 .map { |x| File.join(folder, x) }
+
+    entries.each do |item|
+      if File.directory?(item)
+        scan(item)
+        else
+        puts item
+      end
+    end
+  end
+  def files
+    Dir['*'].reject { |x| x.include? '.' }.each do |folder|
+      puts "Каталок: #{folder}\nCодержит следующие файлы:"
+      scan(folder)
+    end
+  end
+end
