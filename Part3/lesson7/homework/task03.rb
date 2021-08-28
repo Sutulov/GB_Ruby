cd05df048c548fb50f6d31fd096273f6b3511c55
Sutulov <sav.xtx@gmail.com>
2021-08-28 09:05:09 +0500

    first version

--- Part3/lesson7/homework/task03.rb
+++ Part3/lesson7/homework/task03.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 authors = {
   'Design Patterns in Ruby' => ['Russ Olsen'],
   'Eloquent Ruby' => ['Russ Olsen'],
@@ -9,4 +11,16 @@ authors = {
   'Ruby Performance Optimization' => ['Alexander Dymo'],
   'The Ruby Way' => ['Hal Fulton', 'Andre Arko']
 }
-p authors
+
+new = authors.each_with_object({}) do |(_k, v), hash|
+  v.each do |i|
+    hash[i] = if hash.key? i
+                hash[i] + 1
+              else
+                1
+              end
+  end
+end
+value = new.values.uniq.sort
+sort = value.each_with_object([]) { |i, arr| arr << (new.map { |k, v| k if v == i }).compact.sort }
+p sort.flatten.each_with_object({}) { |i, hash| hash.merge!({ i => new.fetch(i) }) }
