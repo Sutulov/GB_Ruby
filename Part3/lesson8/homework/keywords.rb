8607a7fb5d1b819e472dfdf6afe7655459e1e8df
Sutulov <sav.xtx@gmail.com>
2021-08-29 13:00:55 +0500

    test

--- Part3/lesson8/homework/keywords.rb
+++ Part3/lesson8/homework/keywords.rb
@@ -1,2 +1,18 @@
+require 'set'
+
 class Keywords
+  def initialize(item)
+    @set = Set.new
+    @set << item.downcase
+  end
+
+  def =(item)
+    print 'Введите keyword: '
+    @set << item.downcase
+  end
 end
+
+print 'Введите keyword: '
+keywords = Keywords.new(gets.chomp)
+
+p keywords
