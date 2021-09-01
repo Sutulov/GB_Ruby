c24646eab5e926c321421a9995d1153977086f31
Sutulov <sav.xtx@gmail.com>
2021-09-01 18:50:23 +0500

    test

--- Part3/lesson8/homework/warship.rb
+++ Part3/lesson8/homework/warship.rb
@@ -3,8 +3,8 @@ submarine = []
 missile_cruiser = []
 military_transport = []
 (0..2).each do
-  submarine << OpenStruct.new(missiles: rand(20), torpedoes: rand(20), cell: [rand(10), rand(10)],
-  missile_cruiser << OpenStruct.new missiles: rand(20),
+  submarine << OpenStruct.new(missiles: rand(20), torpedoes: rand(20), cell: [rand(10), rand(10)]),
+  missile_cruiser << OpenStruct.new missiles: rand(20)),
   military_transport << OpenStruct.new cargo_hold: 1000, crane: 500
 end
 p submarine
