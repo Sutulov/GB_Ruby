5cc2ca82967169adb1d20e0c5d05e26c41395a62
Sutulov <sav.xtx@gmail.com>
2021-09-01 18:48:41 +0500

    test

--- Part3/lesson8/homework/warship.rb
+++ Part3/lesson8/homework/warship.rb
@@ -1,9 +1,11 @@
 require 'ostruct'
-
-(0..2).each_with_object(OpenStruct.new) do |ships|
-  ships.submarine (OpenStruct.new missiles: rand(20), torpedoes: rand(20), cell: [rand(10), rand(10)])
-  # missile_cruiser: (OpenStruct.new missiles: rand(20)),
-  # military_transport: (OpenStruct.new cargo_hold: 1000, crane: 500)
+submarine = []
+missile_cruiser = []
+military_transport = []
+(0..2).each do
+  submarine << OpenStruct.new(missiles: rand(20), torpedoes: rand(20), cell: [rand(10), rand(10)],
+  missile_cruiser << OpenStruct.new missiles: rand(20),
+  military_transport << OpenStruct.new cargo_hold: 1000, crane: 500
 end
 p submarine
 p missile_cruiser
