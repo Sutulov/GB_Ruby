03039e56870475cf325043925ff2f447656fc06c
Sutulov <sav.xtx@gmail.com>
2021-09-02 12:59:44 +0500

    after rubocop

--- Part3/lesson8/homework/warship.rb
+++ Part3/lesson8/homework/warship.rb
@@ -1,24 +1,19 @@
+# frozen_string_literal: true
+
 require 'ostruct'
 require 'set'
 
 address = Set.new
-while address.size < 9 do
-  address << [rand(1..10), rand(1..10)]
-end
+address << [rand(1..10), rand(1..10)] while address.size < 9
 new = address.to_a
-p address
- 
-def self.cells
-  [rand(1..10), rand(1..10)]
-end
 
 arr = (0..2).each_with_object([]) do |i, ships|
   ships.concat([
-    ('submarine_' +  (i + 1).to_s) => OpenStruct.new(missiles: rand(20), torpedoes: rand(20), cell: cells),
-    ('missile_cruiser_' +  (i + 1).to_s) => OpenStruct.new(missiles: rand(20), cell: cells),
-    ('military_transport_' +  (i + 1).to_s) => OpenStruct.new(cargo_hold: 1000, crane: 500, cell: cells)
-  ])
+                 "submarine_#{i + 1}" => OpenStruct.new(missiles: rand(20), torpedoes: rand(20),
+                                                        cell: new.pop),
+                 "missile_cruiser_#{i + 1}" => OpenStruct.new(missiles: rand(20), cell: new.pop),
+                 "military_transport_#{i + 1}" => OpenStruct.new(cargo_hold: 1000, crane: 500, cell: new.pop)
+               ])
 end
-
 
 p arr
