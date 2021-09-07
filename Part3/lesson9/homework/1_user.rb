c39d76d9a4682b70df81c9c9a6247afc30080e81
Sutulov <sav.xtx@gmail.com>
2021-09-07 15:30:38 +0500

    test

--- Part3/lesson9/homework/1_user.rb
+++ Part3/lesson9/homework/1_user.rb
@@ -7,12 +7,18 @@ end
 class User
   attr_accessor :surname, :name, :patronymic, :email
 
-  raise UserException.new ('Ошибка') if p @email
-  p self::email
+  def initialize
+    yield self
+
+    raise UserException.new('Ошибка!!!!!!') if email == 'pochta@ya.ru'
+  end
 end
 
-first = User.new
-
-first.email = 'Alex'
+first = User.new do |user|
+  user.surname = 'Петров'
+  user.name = 'Иван'
+  user.patronymic = 'Владимирович'
+  user.email = 'pochta@ya.ru'
+end
 
 p first
