ef76610d397c563b0cb17110f54eec0828d0d78d
Sutulov <sav.xtx@gmail.com>
2021-09-07 20:46:52 +0400

    preparation

---
+++ Part3/lesson9/homework/2_solar_system.rb
@@ -0,0 +1,26 @@
+class NotExistingPlanetException < RuntimeError
+end
+PLANETS ={
+  mercury: 1,
+  venus: 2,
+  earth: 3,
+  mars: 4,
+  jupiter: 5,
+  saturn: 6,
+  uranus: 7,
+  neptune: 8,
+  pluto: 9
+}.freeze
+
+class SolarSystem
+  PLANETS.each do |name, value|
+    define_method(name) do
+      value
+    end
+  end
+end
+
+planets = SolarSystem.new
+p planets.mars
+p planets.pluto
+
