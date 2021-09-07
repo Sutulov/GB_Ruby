db8a8683fb53ef83091dd50c6a211cb1d1456eca
Sutulov <sav.xtx@gmail.com>
2021-09-07 20:27:40 +0400

    after rubocop

--- Part3/lesson9/homework/1_user.rb
+++ Part3/lesson9/homework/1_user.rb
@@ -1,22 +1,22 @@
 # frozen_string_literal: true
 
 # Class UserException
-class UserException < Exception
+class UserException < RuntimeError
 end
 
 # Class defining users
 class User
-  VALID_EMAIL_REGEX = /\A[\w+\-]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
+  VALID_EMAIL_REGEX = /\A[\w+\-]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i.freeze
   attr_accessor :surname, :name, :patronymic, :email
 
   def initialize
     yield self
-    raise UserException.new('Ошибка ввода!') if check
+    raise UserException, 'Ошибка ввода!' if check
   end
 
   def check
     str = surname + name + patronymic
-    str[/[a-zA-Z0-9_\-+ ]/] || !(email =~ VALID_EMAIL_REGEX)
+    str[/[a-zA-Z0-9_\-+ ]/] || email !~ VALID_EMAIL_REGEX
   end
 end
 
