b3217ce674fbef3ccfe6035bc0dcd0299adf2937
Sutulov <sav.xtx@gmail.com>
2021-09-09 18:46:12 +0500

    test

--- Part3/courseworks/1_splitting.rb
+++ Part3/courseworks/1_splitting.rb
@@ -1,8 +1,7 @@
 puts file_name = 'test.pdf'
-# data = Array.new()
-new_size = File.size(file_name) / 10
+p new_size = File.size(file_name) / 10
 file_num = 0
-bytes    = 0
+# bytes    = 0
 
 puts 'File exists: ' + File.exist?(file_name).to_s
 
@@ -10,17 +9,20 @@ def write(num, data, name)
   File.open("#{name}x0#{num}", 'w') {|f| f.write data.join}
 end
 
-file = File.open(file_name,"r")
-file.each_with_object([]) { |line, data|          
-     bytes += line.size
-     data << line    
+# file = File.open(file_name,"r")
+# file.each_with_object([]) { |line, data|          
+#      bytes += line.size
+#      data << line    
 
-        if bytes >= new_size  
-          bytes = 0
-          file_num += 1
-          write(file_num, data, file_name)
-          data.clear
-        elsif file.eof?
-          write(file_num + 1, data, file_name)
-        end
-}
+#         if bytes >= new_size  
+#           bytes = 0
+#           file_num += 1
+#           write(file_num, data, file_name)
+#           data.clear
+#         elsif file.eof?
+#           write(file_num + 1, data, file_name)
+#         end
+# }
+str = File.read(file_name)
+data = str[0, 246712]
+write(file_num, data, file_name)
