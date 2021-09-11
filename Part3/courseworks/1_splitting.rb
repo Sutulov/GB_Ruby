070233acc94c62b5d3f9a34f79f17a592902c41a
Sutulov <sav.xtx@gmail.com>
2021-09-11 09:39:47 +0500

    after rubocop

--- Part3/courseworks/1_splitting.rb
+++ Part3/courseworks/1_splitting.rb
@@ -1,16 +1,19 @@
+# frozen_string_literal: true
+
 puts file_name = 'test.txt'
-new_size = File.size(file_name) / 10
-step = 0
+puts "File exists: #{File.exist? file_name}"
+if File.exist? file_name
+  new_size = File.size(file_name) / 10
+  step = 0
 
-puts 'File exists: ' + File.exist?(file_name).to_s
+  def write(num, data, name)
+    File.open(format("#{name}x%02d", num), 'w') { |f| f.write data }
+  end
 
-def write(num, data, name)
-  File.open("#{name}x%02d"% num, 'w') {|f| f.write data }
+  p str = File.read(file_name)
+  (0..9).each do |i|
+    p data = str[step, new_size]
+    write(i + 1, data, file_name)
+    step += new_size
+  end
 end
-
-p str = File.read(file_name)
-(0..9).each_with_index { |i|
-  p data = str[step, new_size]
-  write(i+1, data, file_name)
-  step += new_size
-}
