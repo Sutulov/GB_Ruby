d71da29c672bbed0e3e507e8f4617d70137c6c31
Sutulov <sav.xtx@gmail.com>
2021-09-11 09:09:19 +0500

    new version

--- Part3/courseworks/1_splitting.rb
+++ Part3/courseworks/1_splitting.rb
@@ -1,35 +1,16 @@
 puts file_name = 'test.txt'
 new_size = File.size(file_name) / 10
-file_num = 0
-bytes    = 0
-start = 0
+step = 0
 
 puts 'File exists: ' + File.exist?(file_name).to_s
 
 def write(num, data, name)
-  File.open("#{name}x0#{num}", 'w') {|f| f.write data }
+  File.open("#{name}x%02d"% num, 'w') {|f| f.write data }
 end
 
-# file = File.open(file_name,"r")
-# file.each_with_object([]) { |line, data|          
-#      bytes += line.size
-#      data << line    
-
-#         if bytes >= new_size  
-#           bytes = 0
-#           file_num += 1
-#           write(file_num, data, file_name)
-#           data.clear
-#         elsif file.eof?
-#           write(file_num + 1, data, file_name)
-#         end
-# }
 p str = File.read(file_name)
 (0..9).each_with_index { |i|
-  file_num += 1
-  p data = str[start, new_size]
-  write(file_num, data, file_name)
-  start += new_size
+  p data = str[step, new_size]
+  write(i+1, data, file_name)
+  step += new_size
 }
-
-# write(file_num, data, file_name)
