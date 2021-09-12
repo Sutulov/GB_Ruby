700a47e9a8b4f83798f985502fb4f0af644b0c39
Sutulov <sav.xtx@gmail.com>
2021-09-12 12:42:08 +0500

    final version

--- Part3/courseworks/1_splitting.rb
+++ Part3/courseworks/1_splitting.rb
@@ -2,15 +2,16 @@
 
 puts file_name = 'test.txt'
 puts "File exists: #{File.exist? file_name}"
+raise 'The file must be more than 10 bytes!' if size = File.size(file_name) < 10
 if File.exist? file_name
-  new_size = File.size(file_name) / 10
+  p new_size = (File.size(file_name) / 10.0).ceil
   step = 0
 
   def write(num, data, name)
     File.open(format("#{name}x%02d", num), 'w') { |f| f.write data }
   end
 
-  p str = File.read(file_name)
+  str = File.read(file_name)
   (0..9).each do |i|
     p data = str[step, new_size]
     write(i + 1, data, file_name)
