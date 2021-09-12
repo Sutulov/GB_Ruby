e5f2abd2492d537d5dff208c03fe1b5c7e732c2c
Sutulov <sav.xtx@gmail.com>
2021-09-12 12:45:33 +0500

    after rubocop

--- Part3/courseworks/1_splitting.rb
+++ Part3/courseworks/1_splitting.rb
@@ -2,9 +2,10 @@
 
 puts file_name = 'test.txt'
 puts "File exists: #{File.exist? file_name}"
-raise 'The file must be more than 10 bytes!' if size = File.size(file_name) < 10
+raise 'The file must be more than 10 bytes!' if (size = File.size(file_name)) < 10
+
 if File.exist? file_name
-  p new_size = (File.size(file_name) / 10.0).ceil
+  new_size = (size / 10.0).ceil
   step = 0
 
   def write(num, data, name)
@@ -13,7 +14,7 @@ if File.exist? file_name
 
   str = File.read(file_name)
   (0..9).each do |i|
-    p data = str[step, new_size]
+    data = str[step, new_size]
     write(i + 1, data, file_name)
     step += new_size
   end
