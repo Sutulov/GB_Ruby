29e0a386b3ef6fb5cb5d837ffaa12d3a4b08dc8b
Sutulov <sav.xtx@gmail.com>
2021-09-25 08:40:48 +0500

    test

--- Part3/courseworks/2_date.rb
+++ Part3/courseworks/2_date.rb
@@ -2,11 +2,4 @@
 
 require 'exifr/jpeg'
 
-<<<<<<< HEAD
 puts EXIFR::JPEG.new('lib/test.jpeg').date_time
-=======
-puts EXIFR::JPEG.new('lib/ruby.jpg').date_time
-p EXIFR::JPEG.new('lib/image.jpg').date_time
-puts EXIFR::JPEG.new('lib/ruby.jpg').width
-p EXIFR::JPEG.new('lib/large.jpg').date_time
->>>>>>> main
