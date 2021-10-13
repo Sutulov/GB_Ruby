1051a47e20be97df0015e43cf54d673e2d8699a2
Sutulov <sav.xtx@gmail.com>
2021-10-14 00:09:22 +0400

    a little better

--- Part3/courseworks/1_splitting.rb
+++ Part3/courseworks/1_splitting.rb
@@ -1,18 +1,21 @@
 # frozen_string_literal: true
 
-puts file_name = 'test1.txt'
+puts file_name = 'test.txt'
 puts "File exists: #{File.exist? file_name}"
-raise 'The file must be more than 10 bytes!' if File.exist? file_name || (size = File.size(file_name)) < 10
+
+def check(name)
+  !File.exist? name || File.size(file_name) < 10
+end
+
+raise 'The file must exist and it must be more than 10 bytes!' if check(file_name)
 
 def write(num, data, name)
   File.open(format("#{name}x%02d", num), 'w') { |f| f.write data }
 end
 
 
-new_size = (size / 10.0).ceil
+new_size = (File.size(file_name) / 10.0).ceil
 step = 0
-
-  
 
 str = File.read(file_name)
   (0..9).each do |i|
