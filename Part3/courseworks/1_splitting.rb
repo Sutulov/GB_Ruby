aee11dbcb0a959d2893fbfb5556578d64103a790
Sutulov <sav.xtx@gmail.com>
2021-10-13 23:53:01 +0400

    remove unnecessary

--- Part3/courseworks/1_splitting.rb
+++ Part3/courseworks/1_splitting.rb
@@ -4,18 +4,19 @@ puts file_name = 'test1.txt'
 puts "File exists: #{File.exist? file_name}"
 raise 'The file must be more than 10 bytes!' if File.exist? file_name || (size = File.size(file_name)) < 10
 
-if File.exist? file_name
-  new_size = (size / 10.0).ceil
-  step = 0
+def write(num, data, name)
+  File.open(format("#{name}x%02d", num), 'w') { |f| f.write data }
+end
 
-  def write(num, data, name)
-    File.open(format("#{name}x%02d", num), 'w') { |f| f.write data }
-  end
 
-  str = File.read(file_name)
+new_size = (size / 10.0).ceil
+step = 0
+
+  
+
+str = File.read(file_name)
   (0..9).each do |i|
     data = str[step, new_size]
     write(i + 1, data, file_name)
     step += new_size
   end
-end
