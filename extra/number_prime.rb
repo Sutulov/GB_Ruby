20d17ad3346a4de4d34f8c683c19995ab46b6315
Sutulov <sav.xtx@gmail.com>
2021-10-23 01:01:30 +0400

    a little better

--- extra/number_prime.rb
+++ extra/number_prime.rb
@@ -1,12 +1,9 @@
 # frozen_string_literal: true
+CONST = 100_000
 
 # Test if number is prime
 def prime(num)
-  n = if num > 100_000
-        100_000
-      else
-        num
-      end
+  n = num > CONST ? CONST : num
 
   (2...n).each do |d|
     return false if (num % d).zero?
