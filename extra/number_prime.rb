10201cd82e395e713cdceecb9d3b7f10173c1fab
Sutulov <sav.xtx@gmail.com>
2021-10-24 23:59:11 +0400

    test

--- extra/number_prime.rb
+++ extra/number_prime.rb
@@ -1,16 +1,10 @@
 # frozen_string_literal: true
 
-CONST = 100_000
+require 'prime'
 
 # Test if number is prime
 def prime(num)
-  n = num > CONST ? CONST : num
-
-  (2...n).each do |d|
-    return false if (num % d).zero?
-  end
-
-  num > 1
+  num.prime?
 end
 
 puts prime(gets.chomp.to_i)
