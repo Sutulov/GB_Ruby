ecff7a0348754cde750b9e93eb0457e628fdfad9
Sutulov <sav.xtx@gmail.com>
2021-10-30 22:14:55 +0400

    first version

--- extra/count_smileys.rb
+++ extra/count_smileys.rb
@@ -4,6 +4,19 @@ def count_smileys(arr)
   if arr.empty?
     0
   else
-    1
+    arr.reduce(0) do |m, i|
+      if i[0] == ';' || i[0] == ':'
+        if i.size == 3
+          m += 1 if (i[1] == '-' || i[1] == '~') && (i[2] == ')' || i[2] == 'D')
+        elsif i.size == 2 && (i[1] == ')' || i[1] == 'D')
+          m += 1
+        end
+      end
+      m
+    end
   end
 end
+
+arr = [';D', ':-(', ':-)', ';~)']
+
+p count_smileys(arr)
