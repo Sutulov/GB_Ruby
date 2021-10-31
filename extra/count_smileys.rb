95356a0003ec6d98e84dfdece098f95baa33650d
Sutulov <sav.xtx@gmail.com>
2021-10-31 12:29:09 +0500

    final version

--- extra/count_smileys.rb
+++ extra/count_smileys.rb
@@ -1,17 +1,7 @@
 # frozen_string_literal: true
 
 def count_smileys(arr)
-  arr.reduce(0) do |m, i|
-    if i.start_with?(';', ':') && i.end_with?(')', 'D')
-      case i.size
-      when 3
-        m += 1 if i[1] == '-' || i[1] == '~'
-      when 2
-        m += 1
-      end
-    end
-    m
-  end
+  arr.count { |e| e =~ /[:;]{1}[-~]?[)D]/ }
 end
 
 arr = [';D', ':-(', ':-)', ';~)']
