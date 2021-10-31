3cd0729f6fd214e5722dfe17f603a088b1a3b2a5
Sutulov <sav.xtx@gmail.com>
2021-10-31 16:27:18 +0500

    final version

--- extra/unique.rb
+++ extra/unique.rb
@@ -1,12 +1,9 @@
+# frozen_string_literal: true
+
 def unique_in_order(iterable)
-  case iterable
-  when Array
-    iterable.uniq
-  when String
-    iterable.split('').uniq
-  end
+  (iterable.is_a?(String) ? iterable.chars : iterable).chunk { |x| x }.map(&:first)
 end
 
 p unique_in_order('AAAABBBCCDAABBB')
 p unique_in_order('ABBCcAD')
-p unique_in_order([1,2,2,3,3])
+p unique_in_order([1, 2, 2, 3, 3])
