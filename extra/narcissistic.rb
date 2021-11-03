2ddaed036f227b53b0341de035ea7b4852ce8e86
Sutulov <sav.xtx@gmail.com>
2021-11-03 21:31:47 +0400

    after rubocop

--- extra/narcissistic.rb
+++ extra/narcissistic.rb
@@ -1,5 +1,7 @@
+# frozen_string_literal: true
+
 def narcissistic?(value)
-  value.to_i == (value.split'').reduce(0) { |sum, num| sum += num.to_i**value.size }
+  value.to_i == (value.split '').reduce(0) { |sum, num| sum + num.to_i**value.size }
 end
 
 puts narcissistic?(gets.chomp)
