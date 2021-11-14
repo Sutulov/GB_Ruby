5c757473fa020582445165af8e1614d82232591f
Sutulov <sav.xtx@gmail.com>
2021-11-14 23:49:01 +0400

    test

--- extra/rgb.rb
+++ extra/rgb.rb
@@ -1,3 +1,16 @@
 def rgb(r, g, b)
-  r.to_s(16).upcase + r.to_s(16).upcase + b.to_s(16).upcase
+  def transform(num)
+    if num < 0
+      mum = '00'
+    elsif num > 255
+      num = 'FF'
+    else
+      num = num.to_s(16).upcase
+      num = '0' + num if num.size < 2
+    end
+    num
+  end
+  transform(r) + transform(g) + transform(b)
 end
+
+puts rgb(0, 0, 0)
