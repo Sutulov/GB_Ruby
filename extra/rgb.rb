35baf1f78dd0a2645f7f4278097874c6bba2268d
Sutulov <sav.xtx@gmail.com>
2021-11-15 00:12:30 +0400

    final version

--- extra/rgb.rb
+++ extra/rgb.rb
@@ -1,16 +1,5 @@
 def rgb(r, g, b)
-  def transform(num)
-    if num < 0
-      mum = '00'
-    elsif num > 255
-      num = 'FF'
-    else
-      num = num.to_s(16).upcase
-      num = '0' + num if num.size < 2
-    end
-    num
-  end
-  transform(r) + transform(g) + transform(b)
+  '%02X' * 3 % [r, g, b].map{ |x| x.clamp(0, 255) }
 end
 
-puts rgb(300, 0, 18)
+puts rgb(300, -2, 18)
