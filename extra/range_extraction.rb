e52d3ed6720ad38a5b24fd7b8e39d5eeb054f7fd
Sutulov <sav.xtx@gmail.com>
2021-11-16 17:42:34 +0500

    final version

--- extra/range_extraction.rb
+++ extra/range_extraction.rb
@@ -1,9 +1,7 @@
+# frozen_string_literal: true
+
 def solution(list)
-  new = ''
-  list.each_with_index { |num, i|
-    p new += num.to_s if list[i+1] == nil || (list[i+1].abs - list[i].abs).abs != 1
-  }
-  new
+  list.slice_when { |i, j| i + 1 != j }.to_a.map { |a| a.length < 3 ? a : "#{a.first}-#{a.last}" }.join(',')
 end
 
 arr = [-6, -3, -2, -1, 0, 1, 3, 4, 5, 7, 8, 9, 10, 11, 14, 15, 17, 18, 19, 20]
--- extra/rgb.rb
+++ extra/rgb.rb
@@ -1,5 +1,7 @@
+# frozen_string_literal: true
+
 def rgb(r, g, b)
-  '%02X' * 3 % [r, g, b].map{ |x| x.clamp(0, 255) }
+  '%02X' * 3 % [r, g, b].map { |x| x.clamp(0, 255) }
 end
 
 puts rgb(300, -2, 18)
