db6e93c5a11d5fd0318dab590c04e6a7dcb8e8ae
Sutulov <sav.xtx@gmail.com>
2021-11-18 16:23:55 +0500

    a little bitter

--- extra/pick_peaks.rb
+++ extra/pick_peaks.rb
@@ -1,20 +1,22 @@
 def pick_peaks(arr)
   has = {'pos' => [], 'peaks' => []}
-  arr.chunk_while {|i, j| i <= j }.to_a.reduce(0) { |sum, a|
-    a.uniq!
-    sum = sum + a.length
-    if a.length > 1
-      has['peaks'] << a.last
-      has['pos'] << sum - 1
+  if arr.length > 1
+    arr.chunk_while {|i, j| i <= j }.to_a.reduce(0) { |sum, a|
+      a.uniq!
+      sum = sum + a.length
+      if a.length > 1
+        has['peaks'] << a.last
+        has['pos'] << sum - 1
+      end
+      sum
+    }
+    if arr.last >= arr[arr.length - 2]
+      has['pos'].pop
+      has['peaks'].pop
     end
-    sum
-  }
-  if arr.last >= arr[arr.length - 2]
-    has['pos'].pop
-    has['peaks'].pop
   end
   has
 end
 
-numbers = [3, 2, 3, 6, 4, 1, 2, 3, 2, 1, 2, 3]
+numbers = []
 p pick_peaks(numbers)
