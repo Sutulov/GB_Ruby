26eb595151065e142409e73af0ec41800cb45940
Sutulov <sav.xtx@gmail.com>
2021-11-18 13:01:00 +0500

    test

--- extra/pick_peaks.rb
+++ extra/pick_peaks.rb
@@ -1,13 +1,15 @@
 def pick_peaks(arr)
-  has = {pos: [], peaks: []}
+  has = {'pos' => [], 'peaks' => []}
   arr.chunk_while {|i, j| i <= j }.to_a.reduce(0) { |sum, a|
     sum = sum + a.length
     if a.length > 1
-      has[:pos] << a.last
-      has[:peaks] << sum - 1
+      has['peaks'] << a.last
+      has['pos'] << sum - 1
     end
     sum
   }
+  has['pos'].pop
+  has['peaks'].pop
   has
 end
 
