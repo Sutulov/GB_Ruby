abf8257bc50062db52258ba6a7e2b5e7c0779ade
Sutulov <sav.xtx@gmail.com>
2021-11-18 17:42:26 +0500

    first version

--- extra/pick_peaks.rb
+++ extra/pick_peaks.rb
@@ -1,15 +1,18 @@
+# frozen_string_literal: true
+
 def pick_peaks(arr)
-  has = {'pos' => [], 'peaks' => []}
+  has = { 'pos' => [], 'peaks' => [] }
   if arr.length > 1
-    arr.chunk_while {|i, j| i <= j }.to_a.reduce(0) { |sum, a|
-      a.uniq!
-      sum = sum + a.length
-      if a.length > 1
+    arr.chunk_while { |i, j| i <= j }.to_a.reduce(0) do |sum, a|
+      sum += a.length
+      sum -= (a.count(a.last) - 1) if a.count(a.last) > 1
+      if a.length > 1 && a.uniq.length > 1
         has['peaks'] << a.last
         has['pos'] << sum - 1
       end
+      sum += (a.count(a.last) - 1) if a.count(a.last) > 1
       sum
-    }
+    end
     if arr.last >= arr[arr.length - 2]
       has['pos'].pop
       has['peaks'].pop
@@ -18,5 +21,5 @@ def pick_peaks(arr)
   has
 end
 
-numbers = []
+numbers = [3, 3, 2, 2, 1, 3, 3, 3, 3, 2, 1, 5, 5, 1, 2, 2, 2, 2, 33]
 p pick_peaks(numbers)
