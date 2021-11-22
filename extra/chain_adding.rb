173628a15ef3ba2d194c3187316b19fcbeaa7090
Sutulov <sav.xtx@gmail.com>
2021-11-22 18:50:00 +0500

    test

--- extra/chain_adding.rb
+++ extra/chain_adding.rb
@@ -1,6 +1,8 @@
 class Integer
     def call(n)
-      self + n
+      a = self + n
+      p a
+      a
     end
   end
 
@@ -19,14 +21,26 @@ class Chain
     @n += n
   end
 
+  def +(n)
+    @n += n
+  end
+
 end
 def add(n)
   @x ? @x.sum(n) : @x = Chain.new(n)
   @x::n
+  @x
 end
 
 # puts add(gets.chomp.to_i)
-p add(1).(5).(3) == 9;
-p add(1).(2).(1).(5)
+# p add(1).(5).(3) == 9;
+
+# p add(1).(2).(1).(5)
+# p add(5)
 # f = add(1)
 # p f
+addTwo = add(2)
+p addTwo
+p addTwo + 5
+p addTwo(3)
+# addTwo(3).(5)
