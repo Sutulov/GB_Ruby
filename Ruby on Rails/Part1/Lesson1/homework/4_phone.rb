760b32a85ca9a2545aff337d7a6595e28fef0c51
Sutulov <sav.xtx@gmail.com>
2021-11-26 21:05:25 +0400

    first version

--- Ruby on Rails/Part1/Lesson1/homework/4_phone.rb
+++ Ruby on Rails/Part1/Lesson1/homework/4_phone.rb
@@ -1,9 +1,6 @@
 # frozen_string_literal: true
 
 require 'httparty'
-require 'nokogiri'
-require 'json'
 
 response = HTTParty.get('https://gb.ru/courses')
-doc = response.body.scan(/8\d\d\d\d\d\d\d\d\d+/)
-p doc
+puts response.body.scan(/\+7\d\d\d+/)
