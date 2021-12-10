c9fd286928298ce3704fd3bf17c3e9e19fb04c30
Sutulov <sav.xtx@gmail.com>
2021-12-10 12:46:43 +0400

    final version

--- Ruby on Rails/Part1/Lesson3/homework/02_autoload_lib/lib/http/gb.rb
+++ Ruby on Rails/Part1/Lesson3/homework/02_autoload_lib/lib/http/gb.rb
@@ -3,6 +3,6 @@
 # Class Gb
 class Gb
   def self.count
-    Faraday.get('https://gb.ru/courses').body.lines.reduce(0) { |sum, _i| sum + 1 }
+    Faraday.get('https://gb.ru/courses').body.lines.count
   end
 end
