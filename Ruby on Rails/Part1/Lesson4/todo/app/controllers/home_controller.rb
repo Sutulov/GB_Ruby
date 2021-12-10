04ed6ea86ca3377f80f137bd2e6eebe27f5d49a3
Sutulov <sav.xtx@gmail.com>
2021-12-10 11:51:07 +0400

    test

--- Ruby on Rails/Part1/Lesson4/todo/app/controllers/home_controller.rb
+++ Ruby on Rails/Part1/Lesson4/todo/app/controllers/home_controller.rb
@@ -1,5 +1,9 @@
 class HomeController < ApplicationController
   def index
+    Rails.logger.info '@@@@@@@@@@@@@@@@@@@@@@@@@'
+    Rails.logger.info Rails.inspect
+    Rails.logger.info 'Мы находимся в HomeController#index'
+    Rails.logger.info '@@@@@@@@@@@@@@@@@@@@@@@@@'
     render plain: 'hello'
   end
 end
