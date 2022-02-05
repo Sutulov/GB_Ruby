58cd836badbcd4b523b6a265bbad194b162c1ca0
Sutulov <sav.xtx@gmail.com>
2022-02-05 22:12:10 +0400

    use be_between

---
+++ rspec/between_spec.rb
@@ -0,0 +1,5 @@
+RSpec.describe 'Number' do
+  it 'must be in the range' do
+    expect(5).to be_between(1, 10)
+  end
+end
--- rspec/example_spec.rb
+++ rspec/example_spec.rb
@@ -1,5 +1,10 @@
-RSpec.describe 'Пример' do
-  it 'начинает работать когда' do
-    expect(false).to be_falsy
+RSpec.describe 'Example' do
+  describe 'should work in case of' do 
+    it false do
+      expect(false).to be_falsy
+    end
+    it true do
+      expect(true).to be_truthy
+    end
   end
 end
