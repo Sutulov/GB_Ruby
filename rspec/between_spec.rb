d943c4ce62761c682f1212a882ffd9120ece7368
Sutulov <sav.xtx@gmail.com>
2022-02-16 20:37:50 +0400

    after rubocop

--- rspec/between_spec.rb
+++ rspec/between_spec.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 RSpec.describe 'Number' do
   it 'must be in the range' do
     expect(5).to be_between(1, 10)
--- rspec/example_spec.rb
+++ rspec/example_spec.rb
@@ -1,5 +1,7 @@
+# frozen_string_literal: true
+
 RSpec.describe 'Example' do
-  context 'should work in case of' do 
+  context 'should work in case of' do
     it false do
       expect(false).to be_falsy
     end
