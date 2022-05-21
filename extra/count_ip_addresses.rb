4701013d8abe2fe09612aecab39c996e4353f7ab
Sutulov <sav.xtx@gmail.com>
2022-05-21 18:48:39 +0500

    after rubocop

---
+++ extra/count_ip_addresses.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+def ips_between(start, ending)
+  start + ending
+end
--- extra/menu.rb
+++ extra/menu.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
-arr = %w[help about].select! { |i| i.start_with?('a') }
+p(%w[help about].select! { |i| i.start_with?('a') })
 p new = { 'Помощь': 'help', 'О нас': 'about', 'Главная страница': 'root' }
 p new = new.select!.each { |_key, value| value != 'about' }
 new.each do |key, value|
