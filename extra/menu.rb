860d9839416763dcc837cad6880ff7efb7bad92a
Sutulov <sav.xtx@gmail.com>
2022-05-26 22:22:51 +0400

    after rubocop

--- extra/menu.rb
+++ extra/menu.rb
@@ -2,7 +2,7 @@
 
 p(%w[help about].select! { |i| i.start_with?('a') })
 p new = { 'Помощь': 'help', 'О нас': 'about', 'Главная страница': 'root' }
-p new = new.select!.each { |_key, value| value != 'about' }
+# p new = new.select!.each { |_key, value| value != 'about' }
 new.each do |key, value|
   p key.to_s + value
 end
--- extra/score.rb
+++ extra/score.rb
@@ -1,17 +1,21 @@
 # frozen_string_literal: true
 
+def check(num, sum = 0)
+  case num
+  when 5
+    sum += 50 * (dice.count(5) - 3) if dice.count(5) > 3
+    sum += 50 * dice.count(5) if dice.count(5) < 3
+  when 1
+    sum += 100 * (dice.count(1) - 3) + 1000 if dice.count(1) > 2
+    sum += 100 * dice.count(1) if dice.count(1) < 3
+  end
+  sum
+end
+
 def score(dice)
   (1..6).reduce(0) do |sum, num|
     sum += num * 100 if dice.count(num) > 2 && num > 1
-    case num
-    when 5
-      sum += 50 * (dice.count(5) - 3) if dice.count(5) > 3
-      sum += 50 * dice.count(5) if dice.count(5) < 3
-    when 1
-      sum += 100 * (dice.count(1) - 3) + 1000 if dice.count(1) > 2
-      sum += 100 * dice.count(1) if dice.count(1) < 3
-    end
-    sum
+    sum + check(num)
   end
 end
 
